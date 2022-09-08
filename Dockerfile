8781e5cc658c0e5cb4695221f775116df53caca8
Youngkyu OH <toping4445@gmail.com>
2022-09-08 09:05:36 +0900

    temp commit

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
-FROM python:3.7-slim
+FROM python:3.8-slim
 MAINTAINER yelo.blood <yelo.blood@kakaopaycorp.com>
-RUN ln -s /usr/bin/python3 /usr/bin/python
-RUN ln -s /usr/bin/pip3 /usr/bin/pip
 
 COPY . /app
 WORKDIR /app
-RUN pip install -r requirements.txt
+RUN pip3 install -r requirements.txt
 RUN chmod +x /app/s3toredis.py
 
-CMD ["python","/app/s3toredis.py"]
+CMD ["python3","/app/s3toredis.py"]
--- s3toredis.py
+++ s3toredis.py
@@ -1,96 +1,47 @@
-#!/usr/bin/env python
-
-import pyarrow.parquet as pq
-import s3fs
-from rediscluster import RedisCluster
-import logging
-import json
-import sys
+from multiprocessing import Pool
+import multiprocessing as mp
 import time
 import os
+import logging
 import boto3
-from datetime import datetime,timedelta
+import json
+from datetime import datetime
 import snappy
+from rediscluster import RedisCluster
+import awswrangler as wr
 
 logging.basicConfig(level=logging.INFO)
-
-ACCESS_KEY = os.environ.get("ACCESS_KEY")
-SECRET_KEY = os.environ.get("SECRET_KEY")
-SESSION_TOKEN = os.environ.get("SESSION_TOKEN")
-STD_DATE = os.environ.get("STD_DATE", "2022-07-01")
-TTL = os.environ.get("TTL", 3)
-MIN_KEY_VALUE = os.environ.get("MIN_KEY_VALUE", 1)
-MAX_KEY_VALUE = os.environ.get("MAX_KEY_VALUE", 2500000)
+REGION = os.environ.get("REGION","ap-northeast-2")
 BUCKET = os.environ.get("BUCKET", "sagemaker-yelo-test")
-TABLE_NAME = os.environ.get("TABLE_NAME", "rms_features_feast")
-REDIS_HOST = os.environ.get("REDIS_HOST","feast-datahub.0437il.clustercfg.apn2.cache.amazonaws.com")
-REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
+TABLE_NAME = os.environ.get("TABLE_NAME")
+TESTING = os.environ.get("TESTING", 0)
+STD_DATE = os.environ.get("STD_DATE")
 MODEL_NAME = os.environ.get("MODEL_NAME", "rms")
 MODEL_VERSION = os.environ.get("MODEL_VERSION", "v1")
-RUN_EXPIRATION_JOB = os.environ.get("RUN_EXPIRATION_JOB", 0)
-TESTING = os.environ.get("TESTING", 1)
-
-
-
-s3_fs = s3fs.S3FileSystem(key=ACCESS_KEY, secret = SECRET_KEY, token = SESSION_TOKEN)
-logging.info(f"REDIS_HOST : {REDIS_HOST}")
-logging.info(f"TESTING : {str(TESTING)}")
-logging.info(f"RUN_EXPIRATION_JOB : {str(RUN_EXPIRATION_JOB)}")
-
-
-rc = RedisCluster(startup_nodes=[{"host": REDIS_HOST,"port": REDIS_PORT}], decode_responses=False,skip_full_coverage_check=True)
-pipe = rc.pipeline()
-
-session = boto3.Session(
-    region_name = 'ap-northeast-2',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY,
-    aws_session_token=SESSION_TOKEN
-)
-s3 = session.resource('s3')
-bucket = s3.Bucket(BUCKET)
 org_date_format = '%Y-%m-%d'
 converted_date_format = '%Y%m%d'
+REDIS_HOST = os.environ.get("REDIS_HOST","feast-datahub.0437il.clustercfg.apn2.cache.amazonaws.com")
+REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
+PROCESS_NUM = os.environ.get("PROCESS_NUM", 4)
+rc = RedisCluster(startup_nodes=[{"host": REDIS_HOST,"port": REDIS_PORT}], decode_responses=False,skip_full_coverage_check=True)
 
-#TTL 지난 데이터 삭제 로직 추가
-if RUN_EXPIRATION_JOB:
-    expiration_date = (datetime.strptime(STD_DATE, org_date_format) - timedelta(days=3)).strftime(converted_date_format)
-    start = time.time()
+def func(s3_key):
+    c_proc = mp.current_process()
+    print("Running on Process", c_proc.name, "PID", c_proc.pid)
+    child_pipe = rc.pipeline()
 
-    for current_key in range(MIN_KEY_VALUE,MAX_KEY_VALUE):
-        pipe.zremrangebyscore("rms:v1:"+str(current_key),expiration_date,expiration_date)
-
-    result = pipe.execute()
-    end = time.time()
-
-    print(f"expiration job elapsed time : {end - start:.5f} sec")
-    print(f"deleted values cnt : {str(sum(result))}")
-
-prefix = "data/"+TABLE_NAME+"/yyyymmdd="+STD_DATE
-total_size = 0
-total_success_cnt = 0
-flag1 = time.time()
-
-for idx, object in enumerate(bucket.objects.filter(Prefix=prefix)):
-    if TESTING and idx ==1:
-        break
-
-    url = f"s3://{BUCKET}/{str(object.key)}"
-    df = pq.ParquetDataset(
-        url,
-        filesystem=s3_fs).read_pandas().to_pandas()
-    flag2 = time.time()
-    if TESTING:
-        print(f"itr{idx} - elapsed time to read parquet file as df : {flag2 - flag1:.5f} sec")
+    url = f"s3://{BUCKET}/{s3_key}"
+    df = wr.s3.read_parquet(path = url, dataset=False)
 
     # 모델에서 필요한 feature들만 남기기
-    # df = df[[feature_list_for_certain_model_ver]]
-    df.drop(['label', 'event_timestamp'], axis=1)
+
+    columns_to_drop = ['label','event_timestamp']
+    df = df.drop([x for x in columns_to_drop if x in df.columns], axis=1)
     df_json = df.to_json(orient="records")
     json_list = json.loads(df_json)
 
     for idx2, features in enumerate(json_list):
-        if TESTING and idx2 == 1:
+        if TESTING and idx2 == 1000:
             break
 
         yyyymmdd = (datetime.strptime(STD_DATE, org_date_format).strftime(converted_date_format))
@@ -98,16 +49,35 @@ for idx, object in enumerate(bucket.objects.filter(Prefix=prefix)):
         features.pop("pay_account_id", None)
         key = ":".join([MODEL_NAME,MODEL_VERSION,pay_account_id])
         compressed_feature_json = snappy.compress(json.dumps(features))
-        pipe.zadd(key, {compressed_feature_json:yyyymmdd})
+        child_pipe.zadd(key, {compressed_feature_json:yyyymmdd})
 
-    flag3 = time.time()
-    print(f"itr{idx} - elapsed time to convert all features to JSON : {flag3 - flag2:.5f} sec")
-    result = pipe.execute()
-    flag4 = time.time()
-    print(f"itr{idx} - elapsed time for redis job to finish : {flag4 - flag3:.5f} sec")
-    total_success_cnt += sum(result)
+    result = child_pipe.execute()
+    success_cnt = sum(result)
+    print(f"{c_proc.name}-{c_proc.pid} success_cnt = {success_cnt}")
+    return success_cnt
 
-flag5 = time.time()
-print(f"total elapsed time : {flag5 - flag1:.5f} sec")
-print("total_success_cnt : "+str(total_success_cnt))
+if __name__ == '__main__':
 
+    s3_client = boto3.client('s3', region_name=REGION)
+    prefix = f"data/{TABLE_NAME}" #"/yyyymmdd={STD_DATE}"
+    total_success_cnt = 0
+
+    start = time.time()
+    object_list = []
+    objects = s3_client.list_objects_v2(
+        Bucket=BUCKET,
+        Prefix=prefix).get('Contents', [])
+
+    for obj in objects:
+        object_list.append(obj['Key'])
+
+    pool = Pool(PROCESS_NUM)
+    start = time.time()
+    result = pool.map(func, object_list)
+    total_success_cnt = sum(result)
+    elapsed_time = time.time() - start
+    print(f"total elapsed time = {elapsed_time:.5f} sec")
+    print(f"total_success_cnt = {total_success_cnt}")
+
+    pool.close()
+    pool.join()
